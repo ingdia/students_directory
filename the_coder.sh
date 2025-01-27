b1cb53c5fc2594ccb0523ae94897092e4b195a33
KelvinAaron <k.aaron-onu@alustudent.com>
2025-01-27 11:06:27 +0200

    I added a readme and the bash file: the_coder.sh

---
+++ the_coder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Command to make a directory
+mkdir students_directory
+
+#Command to make 3 files
+touch students_directory/cohort-1.txt
+touch students_directory/cohort-2.txt
+touch students_directory/cohort-3.txt
