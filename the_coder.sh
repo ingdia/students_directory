e6f264f2a6d22f26d222a9d6f9822ca1a7f410c6
Victor-VIO <v.idowu@alustudent.com>
2025-01-29 12:22:29 +0100

    Edited the bash script file to add my name to cohort 3

--- the_coder.sh
+++ the_coder.sh
@@ -8,4 +8,4 @@ touch students_directory/cohort-2.txt
 touch students_directory/cohort-3.txt
 echo "Kevine" > students_directory/cohort-1.txt
 echo "NkubaJr" > students_directory/cohort-2.txt
-
+echo "Victor" > students-directory/cohort-3.txt
